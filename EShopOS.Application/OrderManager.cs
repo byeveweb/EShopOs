326849d2a244988027f76de10562beb706e7283c
byeveweb <desarrollowebeva@gmail.com>
2021-01-13 21:38:27 +0100

    ApplicationManager Create

---
+++ EShopOS.Application/OrderManager.cs
@@ -0,0 +1,34 @@
+﻿using EShopOS.Core;
+using EShopOS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopOS.Application
+{
+    public class OrderManager : GenericManager<OrderManager>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context"></param>
+        public OrderManager(ApplicationDbContext context)
+            : base(context)
+        {
+
+        }
+
+        /// <summary>
+        /// Traemos a través del método GetByShopCarId el Id del producto
+        /// </summary>
+        /// <param name="shopCarId">el id del Carrito</param>
+        /// <returns>Carrito del producto</returns>
+        public IQueryable<ShopCar> GetByShopCartId(int shopCarId)
+        {
+            return Context.Set<ShopCar>().Where(e => e.Id == shopCarId);
+        }
+    }
+}
+
---
+++ EShopOS.Application/ProductManager.cs
@@ -0,0 +1,23 @@
+﻿using EShopOS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopOS.Application
+{
+   public class ProductManager : GenericManager<ProductManager>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context"></param>
+        public ProductManager(ApplicationDbContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
+
